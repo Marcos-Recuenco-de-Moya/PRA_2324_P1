d98e1d291222441c2b2544745124f3e623703059
MRECDEM <mrecdem@alumno.upv.es@ldsic-vdi06.upvnet.upv.es>
2023-10-24 01:08:02 +0200

    ListLinked da violacion de segmento - makefile todo correcto

--- ListLinked.h
+++ ListLinked.h
@@ -36,7 +36,7 @@ class ListLinked : public List<T> {
         }else{
           Node<T>* aux = first;
           
-          for(int i=1 ; i<pos ; i++){
+          for(int i=0 ; i<pos ; i++){
             aux = aux->next;
           }
           return aux->data;
@@ -45,25 +45,31 @@ class ListLinked : public List<T> {
 
 
       friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
-        out << list.arr;
+	      Node<T>* aux = list.first;
+		cout << "List => [";
+	      for(int i=0 ; i<list.n ; i++){
+		out << aux->data;
+		aux = aux->next;
+	      }
+	      cout << "]" << endl;
         return out;
       }
 
 
       //miembros heredados de List<T>
 
-      void insert(int pos, T e){
-        if(pos<1 || pos>n){
+      void insert(int pos, T e) override{
+        if(pos<0 || pos>n){
           throw out_of_range("Posición no válida");
         }else{
-	    if(pos==1){
+	    if(pos==0){
                 prepend(e);
-	    }else if(pos==n){
+	    }else if(pos==n-1){
 	        append(e);
 	    }else{
                 Node<T>* aux = first;
           	Node<T>* prevAux = nullptr;
-		Node<T>* nuevo;          	
+		Node<T>* nuevo;		
 
           	for(int i=1 ; i<pos ; i++){
             	prevAux = aux;
@@ -81,19 +87,19 @@ class ListLinked : public List<T> {
       }
 
 
-	void prepend(T e){
+	void prepend(T e) override{
 		Node<T>* aux = first;
 		Node<T>* nuevo;
 
 		first = nuevo;
 		nuevo->data = e;
-		nuevo->next=aux;
+		nuevo->next = aux;
 		delete aux;
 		n++;
 	}
 
 
-	void append(T e){
+	void append(T e) override{
 		Node<T>* aux = first;
 		Node<T>* nuevo;
 
@@ -109,14 +115,14 @@ class ListLinked : public List<T> {
 
 
 
-	int size(){
+	int size() override{
 		return n;
 	}
 	
 
 
 
-	bool empty(){
+	bool empty() override{
 		if(n==0){
 			cout << "La lista esta vacía.\n";
 			return true;
@@ -128,10 +134,10 @@ class ListLinked : public List<T> {
 
 
 
-	int search(T e){
+	int search(T e) override{
 
 		Node<T>* aux = first;
-		for(int i=1 ; i<n ; i++){
+		for(int i=0 ; i<n ; i++){
 			if(aux->data == e){
 				return i;
 			}else{
@@ -143,13 +149,13 @@ class ListLinked : public List<T> {
 
 
 
-	T get(int pos){
+	T get(int pos) override{
 
-		if(pos<1 || pos>n){
+		if(pos<0 || pos>n){
           		throw out_of_range("Posición no válida");
         	}else{
 			Node<T>* aux = first;
-			for(int i=1 ; i<pos ; i++){
+			for(int i=0 ; i<pos ; i++){
 				aux = aux->next;
 			}
 			return aux->data;
@@ -158,11 +164,11 @@ class ListLinked : public List<T> {
 
 
 
-	T remove(int pos){
+	T remove(int pos) override{
 		Node<T>* aux = first;
 		Node<T>* prevAux = nullptr;
 
-		for(int i=1 ; i<pos ; i++){
+		for(int i=0 ; i<pos ; i++){
 			prevAux = aux;
 			aux = aux->next;
 		}
@@ -176,3 +182,4 @@ class ListLinked : public List<T> {
 
 
 };
+
--- makefile
+++ makefile
@@ -6,5 +6,9 @@ bin/testNode: testNode.cpp Node.h
 	mkdir -p bin
 	g++ -o bin/testNode testNode.cpp Node.h
 
+bin/testListLinked: testListLinked.cpp ListLinked.h List.h
+	mkdir -p bin
+	g++ -o bin/testListLinked testListLinked.cpp ListLinked.h
+
 clean:
 	rm -r *.o *.gch bin
